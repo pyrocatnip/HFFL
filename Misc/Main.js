0c480ceac9bd57e0d4ebccf66e46c715aea89348
pyrocatnip <61922556+pyrocatnip@users.noreply.github.com>
2022-01-11 13:39:45 -0500

    Added updated files

---
+++ Misc/Main.js
@@ -0,0 +1,11 @@
+public class Main {
+  public static _void;public static get void() {
+    return Main._void;
+  }
+public static set void(value) {
+    Main._void = value;
+  }
+ main(String, [], _args) {
+    System.out.printIn("Hello World");
+  }
+}
